8b6829c340770836f9caa1a87764f2014faaa614
Nicholas George <olamafive@gmail.com>
2018-01-01 18:53:28 -0600

    Added Project files

---
+++ Assets/Scripts/FollowCursor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mousePosX = Input.mousePosition.x; 
+		float mousePosY = Input.mousePosition.y; 
+		int scrollDistance = 5; 
+		float scrollSpeed = 80;
+		if (mousePosX < scrollDistance) 
+		{ 
+			transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime); 
+		} 
+
+		if (mousePosX >= Screen.width - scrollDistance) 
+		{ 
+			transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime); 
+		}
+
+		if (mousePosY < scrollDistance) 
+		{ 
+			transform.Translate(transform.forward * scrollSpeed * Time.deltaTime); 
+		} 
+
+		if (mousePosY >= Screen.height - scrollDistance) 
+		{ 
+			transform.Translate(transform.forward * -scrollSpeed * Time.deltaTime); 
+		}
+	}
+}
