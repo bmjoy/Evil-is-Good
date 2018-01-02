ef2fd3fc754af4ba239bb24f837082a97af8150a
Nicholas George <olamafive@gmail.com>
2018-01-01 22:48:31 -0600

    Update

---
+++ Assets/Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+
+    public void doquit()
+    {
+        Debug.Log ("has quit game");
+        Application.Quit();
+    }
+}
