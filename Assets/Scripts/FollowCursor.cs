67d8597e4c0165eb0a4c9cebbf3220b9aa75ca81
Nicholas George <olamafive@gmail.com>
2018-01-03 15:48:34 -0600

    Update for 1/3/18

--- Assets/Scripts/FollowCursor.cs
+++ Assets/Scripts/FollowCursor.cs
@@ -2,37 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCursor : MonoBehaviour {
+public class FollowCursor : MonoBehaviour
+{
+    public float value;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float mousePosX = Input.mousePosition.x; 
-		float mousePosY = Input.mousePosition.y; 
-		int scrollDistance = 5; 
-		float scrollSpeed = 80;
-		if (mousePosX < scrollDistance) 
-		{ 
-			transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime); 
-		} 
-
-		if (mousePosX >= Screen.width - scrollDistance) 
-		{ 
-			transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime); 
-		}
-
-		if (mousePosY < scrollDistance) 
-		{ 
-			transform.Translate(transform.forward * scrollSpeed * Time.deltaTime); 
-		} 
-
-		if (mousePosY >= Screen.height - scrollDistance) 
-		{ 
-			transform.Translate(transform.forward * -scrollSpeed * Time.deltaTime); 
-		}
-	}
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y));
+        Vector3 finalPos = new Vector3(Mathf.Round(mousePos.x), Mathf.Round(value), Mathf.Round(mousePos.z));
+        transform.position = finalPos;
+    }
 }
---
+++ Assets/Scripts/FollowCursorSel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursorSel : MonoBehaviour
+{
+    public float value;
+    public float offset;
+    public float grid_scale;
+
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y));
+        Vector3 finalPos = new Vector3(Mathf.Round(mousePos.x + -offset), Mathf.Round(value), Mathf.Round(mousePos.z + offset));
+        transform.position = finalPos;
+        var currentPos = transform.position;
+        transform.position = new Vector3(Mathf.Round(currentPos.x / grid_scale) * grid_scale,
+                                     Mathf.Round(currentPos.y / grid_scale) * grid_scale,
+                                     Mathf.Round(currentPos.z / grid_scale) * grid_scale);
+
+
+    }
+}
