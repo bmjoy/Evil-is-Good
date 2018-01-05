e814ac6d36dbf1d8cbaec468498d88a2d29154bb
Nicholas George <olamafive@gmail.com>
2018-01-04 20:46:04 -0600

    Update 1/4/18

---
+++ Assets/Scroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+    public bool canScroll;
+    public float Value;
+    public float scrollbar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void GetMouseDown () {
+        
+	}
+}
