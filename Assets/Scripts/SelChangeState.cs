998118045f0188104877d4cae4d76624ce5553c5
Nicholas George <olamafive@gmail.com>
2018-01-05 23:14:50 -0600

    Update 1/5/18

---
+++ Assets/Scripts/SelChangeState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelChangeState : MonoBehaviour {
+
+    public GameObject Sel;
+    public GameObject Sel2;
+
+	void Start () {
+        Sel.SetActive(false);
+        Sel2.SetActive(false);
+    }
+	
+	void OnMouseEnter () {
+        Sel.SetActive(true);
+        Sel2.SetActive(false);
+    }
+
+    void OnMouseExit ()
+    {
+        Sel.SetActive(false);
+        Sel2.SetActive(true);
+    }
+}
